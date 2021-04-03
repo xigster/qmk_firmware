0603dfd16f4d3e7ee07d4164b1a3c6b0475fe3f1
Mark Winkelman <mark@xigster.net>
2021-04-03 19:16:20 -0400

    Updated custom keymap for wrong mappings for page down and end

--- keyboards/dztech/dz60rgb_ansi/keymaps/mark-v1/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/mark-v1/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, KC_INS,  _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
         _______, KC_VOLD, KC_MUTE, KC_VOLU, KC_PGUP, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,
-        _______,          _______, _______, KC_PGDN, KC_END,  _______, NK_TOGG, _______, _______, _______, _______,          _______,
+        _______,          _______, _______, _______, KC_PGDN, KC_END,  NK_TOGG, _______, _______, _______, _______,          _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______
     ),
     [2] = LAYOUT_60_ansi(
